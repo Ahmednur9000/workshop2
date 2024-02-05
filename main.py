35896dbd68995e8a717b0cb9d42fd8b2c8336dea
Ahmednur9000 <159029623+Ahmednur9000@users.noreply.github.com>
2024-02-05 14:28:27 +0100

    Update main.py

--- main.py
+++ main.py
@@ -1,7 +1,9 @@
 def main( ):
     name = input('Enter your name')
     print(f'hello = {name}')
-    
+    for ( char name : name) (
+        character = {}:
+        character.add name:
     
 if __name__ == '__main__':
     main()
